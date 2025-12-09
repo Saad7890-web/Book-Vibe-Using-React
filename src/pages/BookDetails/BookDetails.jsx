e8950691cfe7108bab1a4c944f1db5caf0e6c4c5
Saad7890-web <omysaadislam@gmail.com>
2025-12-10 02:34:09 +0600

    book details page done

--- src/pages/BookDetails/BookDetails.jsx
+++ src/pages/BookDetails/BookDetails.jsx
@@ -1,7 +1,18 @@
+import { useLoaderData, useParams } from "react-router";
+
 const BookDetails = () => {
+  const { id } = useParams();
+  const bookId = parseInt(id);
+  const data = useLoaderData();
+  const singleBook = data.find((book) => book.bookId === bookId);
+  const { bookName, image } = singleBook;
   return (
-    <div>
-      <h1>Book details page</h1>
+    <div className="w-2/3 mx-auto">
+      <img className="w-48" src={image} alt="" />
+      <h1>{bookName}</h1>
+
+      <button className="btn btn-accent m-2">Read</button>
+      <button className="btn btn-info m-2">WishList</button>
     </div>
   );
 };
